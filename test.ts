1871bc67e87e77be4f54768785b2dfaca2a48950
Ziv Li <imshikar@gmail.com>
2020-05-01 12:58:26 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
 basic.forever(() => {
-    let p = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Inches);
+    let p = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters);
     led.plotBarGraph(p, 0);
 })
